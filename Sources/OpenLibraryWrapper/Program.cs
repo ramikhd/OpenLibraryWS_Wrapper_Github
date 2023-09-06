a8dc944b34b97fa88c27d0f8dccb48be9fb99446
rakhedair <rami.khedair@etu.uca.fr>
2023-09-06 15:22:21 +0200

    :sparkles: Add swagger

--- Sources/OpenLibraryWrapper/Program.cs
+++ Sources/OpenLibraryWrapper/Program.cs
@@ -8,6 +8,7 @@ using StubbedDTO;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<IDtoManager,OpenLibClientAPI>();
 builder.Services.AddControllers();
@@ -15,6 +16,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
+
+
 
 app.UseHttpsRedirection();
 
